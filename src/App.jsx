be9964e7dc5fc74e7cbf4c36cceb86072ff303e0
rotero08 <roteroerazo2@gmail.com>
2024-06-10 04:59:53 -0500

    fixed layout issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
 import React, { useState } from 'react'
 import './styles.css'
-import { Container, Grid, Box, useTheme, useMediaQuery } from '@mui/material'
+import { Container, Stack } from '@mui/material'
 import CromaticCircle from './components/CromaticCircle'
 import TemporaryDrawer from './components/Sidebar'
 import ResponsivePiano from './components/Piano'
 
 function App() {
-  const theme = useTheme()
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
 
   const [transformation, setTransformation] = useState(1)
   const [selectedNotes, setSelectedNotes] = useState([])
   const [numSelected, setNumSelected] = useState(2)
   const [transformations, setTransformations] = useState([]) // Manage multiple transformations
-
-  const notes = [
-    { note: 'C', num: 0 },
-    { note: 'C♯', num: 1 },
-    { note: 'D', num: 2 },
-    { note: 'E♭', num: 3 },
-    { note: 'E', num: 4 },
-    { note: 'F', num: 5 },
-    { note: 'F♯', num: 6 },
-    { note: 'G', num: 7 },
-    { note: 'G♯', num: 8 },
-    { note: 'A', num: 9 },
-    { note: 'B♭', num: 10 },
-    { note: 'B', num: 11 },
-  ]
 
   // Function to add a new transformation
   const addTransformation = () => {
@@ -41,71 +24,34 @@ function App() {
   }
 
   return (
-    <Container
-      maxWidth={false}
-      sx={{
-        height: 'calc(100vh - 64px)',
-        position: 'relative',
-        display: 'flex',
-        flexDirection: 'column',
-        p: 0,
-        m: 0,
-      }}
-    >
-      <Box
-        sx={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          zIndex: 1200,
-          width: isMobile ? '100%' : 'auto',
-        }}
+    <div className="app-container">
+      <TemporaryDrawer
+        transformation={transformation}
+        setTransformation={setTransformation}
+        numSelected={numSelected}
+        setNumSelected={setNumSelected}
+        transformations={transformations}
+        addTransformation={addTransformation}
+        removeTransformation={removeTransformation}
+      />
+
+      <Stack
+        justifyContent="space-between"
+        className="app-stack"
       >
-        <TemporaryDrawer
-          transformation={transformation}
-          setTransformation={setTransformation}
-          numSelected={numSelected}
-          setNumSelected={setNumSelected}
-          transformations={transformations} // Pass down transformations
-          addTransformation={addTransformation} // Pass down add transformation function
-          removeTransformation={removeTransformation} // Pass down remove transformation function
-        />
-      </Box>
-      <Box
-        sx={{
-          flex: 0,
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          p: isMobile ? 3 : 4,
-        }}
-      >
-        <Grid
-          container
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Grid item xs={12} lg={12}>
-            <CromaticCircle
-              selectedNotes={selectedNotes}
-              setSelectedNotes={setSelectedNotes}
-              numSelected={numSelected}
-              transformations={transformations} // Pass down transformations
-            />
-          </Grid>
-          <Box
-            sx={{
-              width: '100%',
-              position: 'fixed',
-              bottom: 0,
-            }}
-          >
-            <ResponsivePiano selectedNotes={selectedNotes} />
-          </Box>
-        </Grid>
-      </Box>
-    </Container>
+        <div className="center-container">
+          <CromaticCircle
+            selectedNotes={selectedNotes}
+            setSelectedNotes={setSelectedNotes}
+            numSelected={numSelected}
+            transformations={transformations}
+          />
+        </div>
+        <div className="piano-container">
+          <ResponsivePiano selectedNotes={selectedNotes} />
+        </div>
+      </Stack>
+    </div>
   )
 }
 
